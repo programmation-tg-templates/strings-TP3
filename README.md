ac6800ab1c78957c0876437bec3ae6d061555479
François Roland <francois.roland@heh.be>
2025-10-15 09:04:52 +0200

    Update instructions and tests

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
-# Module - TP1 - Titre
+# Chaines de caractères - TP3 - Vérification et formatage d’e-mails
 
 ## Consignes
+
+Écrire un programme qui contient une chaîne prédéfinie représentant une adresse e-mail.
+La fonction `verifierEtFormaterEmail` doit :
+- Vérifier que la chaîne contient exactement un “@” et au moins un point (.) après le “@”.
+- Vérifier que le nom de domaine (après le dernier point) contient au moins 2 caractères.
+- Si l’e-mail est valide, retourner  : "L’adresse <email> est valide."
+- Sinon, retourner : "L’adresse <email> est invalide."
+- Transformer l’e-mail en minuscules pour standardiser le format.
+
+
 
 **Signature de la fonction :**
 
 ```ts
-function fct(param: boolean): boolean;
+function verifierEtFormaterEmail(email: string): string
 ```
 
 ## Exemples
+
+- `verifierEtFormaterEmail("exemple.utilisateur@domaine.com")` doit retourner `L'adresse 'exemple.utilisateur@domaine.com' est valide et dans le bon format.`
+- `verifierEtFormaterEmail("user@@domain.com")` doit retourner `L'adresse 'user@@domain.com' est invalide.`
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,9 +1,39 @@
-// Tests unitaires écrits par l'enseignant. Ne modifiez pas ce fichier.
+import { verifierEtFormaterEmail } from "./index";
 
-import {...} from '../index';
+describe("Vérification et formatage d’e-mails", () => {
+  test("E-mail valide", () => {
+    expect(verifierEtFormaterEmail("exemple.utilisateur@domaine.com")).toEqual(
+      "L'adresse 'exemple.utilisateur@domaine.com' est valide et dans le bon format."
+    );
+  });
 
-describe('...', () => {
-    test('...', () => {
-        expect(false).toBe(true);
-    });
+  test("E-mail avec plusieurs @", () => {
+    expect(verifierEtFormaterEmail("user@@domain.com")).toEqual("L'adresse 'user@@domain.com' est invalide.");
+  });
+
+  test("E-mail sans @", () => {
+    expect(verifierEtFormaterEmail("utilisateur.domaine.com")).toEqual(
+      "L'adresse 'utilisateur.domaine.com' est invalide."
+    );
+  });
+
+  test("E-mail sans point après @", () => {
+    expect(verifierEtFormaterEmail("utilisateur@domainecom")).toEqual(
+      "L'adresse 'utilisateur@domainecom' est invalide."
+    );
+  });
+
+  test("E-mail avec extension trop courte", () => {
+    expect(verifierEtFormaterEmail("utilisateur@domaine.c")).toEqual("L'adresse 'utilisateur@domaine.c' est invalide.");
+  });
+
+  test("E-mail commençant par @", () => {
+    expect(verifierEtFormaterEmail("@domaine.com")).toEqual("L'adresse '@domaine.com' est invalide.");
+  });
+
+  test("E-mail avec majuscules", () => {
+    expect(verifierEtFormaterEmail("User.Name@DOMAIN.COM")).toEqual(
+      "L'adresse 'user.name@domain.com' est valide et dans le bon format."
+    );
+  });
 });
